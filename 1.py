80c04e29799a962f9361c281717e1fc270870fc8
puff <angrypuff333@gmail.com>
2025-06-27 00:44:17 +0800

    x

---
+++ 1.py
@@ -0,0 +1,4 @@
+cookie_dict = self.session.cookies.get_dict()
+cookie_dict['OCB_REMEMBER_2FA'] = self.username
+cookie_dict['KEYCLOAK_REMEMBER_ME'] = "KEYCLOAK_REMEMBER_ME:{}".format(self.username)
+self.session = self.get_new_session()
